ce4f9b340f89ebf0434e041267872da5f1680ba4
Nguyen Viet Hoang (APTECH HN) <hoangnvth2010033@fpt.edu.vn>
2022-02-12 11:45:43 +0700

    SQLite Demo

--- Windows Forms Programming_1/HelloWorld/Demo_SQLite/Pages/CreateTransactionPage.xaml.cs
+++ Windows Forms Programming_1/HelloWorld/Demo_SQLite/Pages/CreateTransactionPage.xaml.cs
@@ -4,6 +4,7 @@ using SQLitePCL;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -15,6 +16,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 
@@ -33,23 +35,34 @@ namespace Demo_SQLite.Pages
            
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private BitmapImage WarningImage()
         {
-
+            BitmapImage img = new BitmapImage(new Uri("ms-appx:///Assets/Images/madPepe.png"));
+            return img;
+        }
+        private void ClearData()
+        {
+            Name.Text = string.Empty;
+            Description.Text = string.Empty;
+            Money.Text = string.Empty;
+            CreatedDate.SelectedDate = null;
+            Category.Text = string.Empty;
         }
 
-        public void them_click(object sender, RoutedEventArgs e)
+        public async void them_click(object sender, RoutedEventArgs e)
         {
-            /*  var personalTransaction = new PersonalTransaction()
-              {
-                  Name = Name.Text,
-                  Description = Description.Text,
-                  Money = Double.Parse(Money.Text),
-                  CreatedDate = DateTime.Parse(CreatedDate.SelectedDate.ToString()),
-                  Category = int.Parse(Category.Text)
-              }; */
-
-            Insert_Data();
+            if(Insert_Data() == true)
+            {
+              
+                ContentDialog message = new ContentDialog();
+                message.Title = "Insert Record";
+                message.Content = "Expenditure '" + Name.Text + "' has been inserted successfully !";
+                message.Foreground = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 71, 101, 10));
+                message.PrimaryButtonText = "OK";
+                var result = await message.ShowAsync();
+                ClearData();
+            }
+           
         }
 
         private bool Insert_Data()
@@ -60,20 +73,129 @@ namespace Demo_SQLite.Pages
                 using (var personstmt = conn.Prepare("INSERT INTO PersonalTransaction (Name,Description,Money,CreatedDate,Category) " +
                     "VALUES (?,?,?,?,?)"))
                 {
-                    personstmt.Bind(1, Name.Text);
-                    personstmt.Bind(2, Description.Text);
-                    personstmt.Bind(3, Money.Text);
-                    personstmt.Bind(4, CreatedDate.SelectedDate.ToString());
-                    personstmt.Bind(5, Category.Text);
+                    //Validate
+                    if(Name.Text == string.Empty)
+                    {
+                        image.Source = WarningImage();
+                        NameErr.Text = "*Expenditure name is required !";
+                        return false;
+                    }
+                    else
+                    {
+                        NameErr.Text = "";
+                        personstmt.Bind(1, Name.Text);
+                    }
+
+                    if (Description.Text == string.Empty)
+                    {
+                        image1.Source = WarningImage();
+                        DesErr.Text = "*Expenditure description is required !";
+                        return false;
+                    }
+                    else
+                    {
+                        DesErr.Text = "";
+                        personstmt.Bind(2, Description.Text);
+                    }
+
+                    double b;
+                    if (Money.Text == string.Empty)
+                    {
+                        image2.Source = WarningImage();
+                        MoneyErr.Text = "*Money is required !";
+                        return false;
+                    }
+                    else if(!double.TryParse(Money.Text.ToString(),out b)){
+                        image2.Source = WarningImage();
+                        MoneyErr.Text = "*Money is a double type !";
+                        return false;
+                    }
+                    else if (double.Parse(Money.Text) <= 0)
+                    {
+                        image2.Source = WarningImage();
+                        MoneyErr.Text = "*Money is a positive type !";
+                        return false;
+                    }
+                    else
+                    {
+                        MoneyErr.Text = "";
+                        personstmt.Bind(3, Money.Text);
+                    }
+
+                    if(CreatedDate.SelectedDate == null)
+                    {
+                        image3.Source = WarningImage();
+                        DateErr.Text = "*Created date must be chosen !";
+                        return false;
+                    }
+                    else if(DateTime.Now < DateTime.Parse(CreatedDate.SelectedDate.ToString()))
+                    {
+                        image3.Source = WarningImage();
+                        DateErr.Text = "*Created date must be less than present !";
+                        return false;
+                    }
+                    else
+                    {
+                        DateErr.Text = "";
+                        personstmt.Bind(4, CreatedDate.SelectedDate.ToString());
+                    }
+
+                    if (Category.Text == string.Empty)
+                    {
+                        image4.Source = WarningImage();
+                        CateErr.Text = "*Expenditure category is required !";
+                        return false;
+                    }
+                    else
+                    {
+                        CateErr.Text = "";
+                        personstmt.Bind(5, Category.Text);
+                    }                  
                     personstmt.Step();
+                   
                 }
                 return true;
             }
             catch (Exception ex)
             {
-                return true;
+                Debug.WriteLine(ex.Message);
+                return false ;
             }
         }
 
+        private void Name_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            image.Source = null;
+            NameErr.Text = "";
+        }
+
+        private void Description_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            image1.Source = null;
+            DesErr.Text = "";
+        }
+
+        private void Money_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            image2.Source = null;
+            MoneyErr.Text = "";
+        }
+
+        private void Category_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            image4.Source = null;
+            CateErr.Text = "";
+        }
+
+        private void CreatedDate_SelectedDateChanged(DatePicker sender, DatePickerSelectedValueChangedEventArgs args)
+        {
+            image3.Source = null;
+            DateErr.Text = "";
+        }
+
+        private void list_page(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Pages.ListTransactionPage));
+        }
     }
 }
---
+++ Windows Forms Programming_1/HelloWorld/Demo_SQLite/Pages/ListTransactionPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Demo_SQLite.Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Data.SQLite;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Demo_SQLite.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ListTransactionPage : Page
+    {
+        public ObservableCollection<PersonalTransaction> Persons { get; set; }
+
+        public ListTransactionPage()
+        {
+           
+            Persons = new ObservableCollection<PersonalTransaction>();
+            DataContext = this;
+            this.InitializeComponent();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await ShowDataList(Persons);
+        }
+
+        public static async Task ShowDataList(ObservableCollection<PersonalTransaction> persons)
+        {
+            using(SQLiteConnection conn = new SQLiteConnection("sqlitepcldemo.db"))
+            {
+                await conn.OpenAsync();
+                SQLiteCommand command = new SQLiteCommand("Select * from PersonalTransaction", conn);
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    var idOrdinal = reader.GetOrdinal("Id");
+                    var nameOrdinal = reader.GetOrdinal("Name");
+                    var desOrdinal = reader.GetOrdinal("Description");
+                    var moneyOrdinal = reader.GetOrdinal("Money");
+                    var dateOrdinal = reader.GetOrdinal("CreatedDate");
+                    var cateOrdinal = reader.GetOrdinal("Category");
+                    //them truong
+                    while (await reader.ReadAsync())
+                    {
+                        persons.Add(new PersonalTransaction() {
+                            ID = reader.GetInt32(idOrdinal),
+                            Name = reader.GetString(nameOrdinal),
+                         Description = reader.GetString(desOrdinal),
+                            Money = reader.GetDouble(moneyOrdinal),
+                            CreatedDate = reader.GetDateTime(dateOrdinal),
+                            Category = reader.GetInt32(cateOrdinal)
+                        });
+                    }
+                }
+               
+            }
+        }
+
+        public async Task<List<PersonalTransaction>> GetTransaction()
+        {
+            List<PersonalTransaction> perList = null;
+            try
+            {
+                var conn = new SQLiteConnection("sqlitepcldemo.db");
+                var query = "select * from PersonalTransaction;";
+                var expenditures = new List<PersonalTransaction>();
+                /* using (var stmt = conn.Prepare(query))
+                 {
+                      while(SQLiteResult.DONE == stmt.Step())
+                      {
+                          perList = new List<PersonalTransaction>()
+                          {
+                              ID = (int)stmt[1],
+                              perList[Name] = (string)stmt[1],
+                              Description = (string)stmt[2],
+                              Money = (double)stmt[3],
+                              CreatedDate = (DateTime)stmt[4],
+                              Category = (int)stmt[5]
+                          }; 
+                      }
+            }*/
+                return perList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
