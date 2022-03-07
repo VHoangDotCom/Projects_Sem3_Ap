c770d4779c7b03f7b59478c60f3dff6c5d904ae8
Nguyen Viet Hoang (APTECH HN) <hoangnvth2010033@fpt.edu.vn>
2022-03-07 22:17:02 +0700

    mvc authorize

---
+++ Web Application Development/AuthenticationMVC/AuthenticationMVC/Data/MyIdentityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AuthenticationMVC.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationMVC.Data
+{
+    public class MyIdentityDbContext : IdentityDbContext<User>
+    {
+        public MyIdentityDbContext() : base("ConnectionString")
+        {
+
+        }
+    }
+}
---
+++ Web Application Development/AuthenticationMVC/AuthenticationMVC/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationMVC.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
